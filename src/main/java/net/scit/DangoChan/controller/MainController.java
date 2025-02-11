4c6def4b74069afa7e3e332c863e54b9fc6873ec
heokreelim-admin <heokree.lim@gmail.com>
2025-02-11 15:52:59 +0900

    コメントのテスト

--- src/main/java/net/scit/DangoChan/controller/MainController.java
+++ src/main/java/net/scit/DangoChan/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	
+	// 주석 테스트, comment test, コメントのテスト（検討）
 	@GetMapping("/")
 	public String index()
 	{
