94bf61e7976e80235e708db5d05571f0c0cd44a0
Yehwal Ahn <umoogking@gmail.com>
2025-02-12 18:13:43 +0900

    Add FlashCardService and Modal test in flashcard.html

---
+++ src/main/java/net/scit/DangoChan/service/FlashCardService.java
@@ -0,0 +1,48 @@
+package net.scit.DangoChan.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlashCardService {
+
+	//private final Repository variable start
+	
+	//private final Repository variable end
+	
+	//category start
+	
+		//AYH start
+		
+		//AYH end
+		
+		//PJB start
+		
+		//PJB end
+	
+	//category end
+	
+	//deck start
+	
+		//AYH start
+		
+		//AYH end
+			
+		//PJB start
+			
+		//PJB end
+	
+	//deck end
+	
+	//card start
+	
+		//AYH start
+		
+		//AYH end
+		
+		//SYH start
+	
+		//SYH end
+	
+	//card end
+	
+}
