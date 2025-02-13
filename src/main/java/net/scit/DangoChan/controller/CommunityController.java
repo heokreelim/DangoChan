3a300e7051abd59be00e0b0ae84c8ac498eaf130
Kim, Sung Yeon <kimjoans@gmail.com>
2025-02-13 15:59:01 +0900

    CommunityController, CommmunityBoardList, CommunityWrite savepoint 2502131

--- src/main/java/net/scit/DangoChan/controller/CommunityController.java
+++ src/main/java/net/scit/DangoChan/controller/CommunityController.java
@@ -1,11 +1,17 @@
 package net.scit.DangoChan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.scit.DangoChan.dto.CommunityDTO;
+import net.scit.DangoChan.service.CommunityService;
 
 @Slf4j
 @Controller
@@ -13,13 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/community")
 public class CommunityController {
 	
+	private final CommunityService communityService;
+	
 	/**
-	 * 게시글 조회
-	 * 
+	 * 게시글 조회 
+	 * @param model 
 	 * @return
 	 * */
 	@GetMapping("/communityBoardList")
-	public String communityBoardList() {
+	public String communityBoardList(
+			@RequestParam(name="searchItem", defaultValue = "boardTitle") String searchItem,
+			@RequestParam(name="searchWord", defaultValue = "") String searchWord,
+			Model model
+			) {
+		
+		// 2) 페이징 기능 + 검색 기능
+
+		// 1) 검색 기능 추가
+		
+		
 		return "community/communityBoardList";
 	}
 
@@ -30,7 +48,30 @@ public class CommunityController {
 	 * @return
 	 * */
 	@GetMapping("/communityWrite")
-	public String boardWrite() {
+	public String communityWrite() {
 		return "community/communityWrite";
 	}
+	
+	/**
+	 * 게시글 등록 요청
+	 * 
+	 * @param communityDTO
+	 * @return
+	 * 
+	 * */
+	@PostMapping("/communityWrite")
+	public String communityWrite(@ModelAttribute CommunityDTO communityDTO) {
+		communityService.insertBoard(communityDTO);
+		
+		return "redirect:/community/communityBoardList";
+	}
+	
+	/**
+	 * 게시글 상세 보기 화면 요청
+	 * 
+	 * @param model
+	 * @return
+	 * 
+	 * */
+	
 }
--- src/main/java/net/scit/DangoChan/service/CommunityService.java
+++ src/main/java/net/scit/DangoChan/service/CommunityService.java
@@ -2,8 +2,14 @@ package net.scit.DangoChan.service;
 
 import org.springframework.stereotype.Service;
 
+import net.scit.DangoChan.dto.CommunityDTO;
+
 @Service
 public class CommunityService {
+
+	public void insertBoard(CommunityDTO communityDTO) {
+		
+	}
 	
 	
 }
