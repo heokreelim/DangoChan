ac4585ac3c2e5b16b4b4190c707f910fd9386f51
PJB <pjb9704@gamil.com>
2025-03-20 22:39:24 +0900

    edit pagination

--- src/main/resources/static/js/communitySearch.js
+++ src/main/resources/static/js/communitySearch.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
+    $('#searchBtn').on('click', function(){
+        $('#requestPage').val(1);      // 검색 시 페이지는 1로 초기화
+        $('#formMode').val('search');  // 검색 모드 설정
+        $('#searchForm').submit();
+    });
+
     $('#searchForm').on('submit', function(){
+        let mode = $('#formMode').val();
         let searchInput = $.trim($("input[name='searchWord']").val());
-        if(searchInput === ''){
+
+        if(mode === 'search' && searchInput === ''){
             alert('검색어를 입력해주세요.');
-            return false;  // 이벤트 전파 및 기본 동작 중지
+            return false;
         }
+
+        // 문제 없으면 폼 전송
     });
 });
+
+function pageFormSubmit(page) {
+    $('#requestPage').val(page);
+    $('#formMode').val('page');  // 페이지 이동 모드 설정
+    $('#searchForm').submit();
+}
